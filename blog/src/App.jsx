bdb3df5e2a3e5aeee239bf210493bfccdc6a8701
ChanGyun Lee <lgyn10@naver.com>
2023-07-14 21:05:27 +0900

    useState 불변성 관리

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // eslint-disable-next-line no-unused-vars
+  const [title, setTitle] = useState(['남자 코트 추천', '김포 맛집', '알고리즘']);
+  const [cnt, setCnt] = useState(0);
+  const click = () => {
+    // 불변성 보장
+    let tmp = [...title]; // 원본에 변형을 가하면 안됨
+    tmp[0] = '여자 코트 추천';
+    setTitle(tmp);
+    setCnt((prev) => prev + 1);
+    console.log(cnt);
+  };
+  function sorting() {
+    let tmp2 = [...title];
+    tmp2.sort((p1, p2) => p1.localeCompare(p2));
+    console.log(tmp2);
+    setTitle(tmp2);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div className='black-nav'>
+          {/* {color:color} -> 작동 안함  */}
+          <h3>BLOG</h3>
+        </div>
+        <div className='list'>
+          <h3>
+            {title[0]}
+            <span style={{cursor: 'pointer'}} onClick={click}>
+              😎
+            </span>
+            {cnt}
+          </h3>
+          <p>7월 14일 발행</p>
+        </div>
+        <div className='list'>
+          <h3>{title[1]}</h3>
+          <p>7월 14일 발행</p>
+        </div>
+        <div className='list'>
+          <h3>{title[2]}</h3>
+          <p>7월 14일 발행</p>
+        </div>
+        <button onClick={sorting}>정렬</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
