eaa75a4c190f36bf6c0dfcbea72c8f36796b5394
ChanGyun Lee <lgyn10@naver.com>
2023-07-16 21:23:11 +0900

    map함수로 컴포넌트화, bind(null,i)로 인자 접근

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types';
 
 function App() {
   const [title, setTitle] = useState(['도서 추천', '김포 맛집', '알고리즘']);
-  const [cnt, setCnt] = useState(0);
+  const [cnt, setCnt] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
 
-  const click = () => {
+  const click = (i) => {
     // 불변성 보장
     let tmp = [...title]; // 원본에 변형을 가하면 안됨
     tmp[0] = '강의 추천';
     setTitle(tmp);
-    setCnt((prev) => prev + 1);
-    console.log(cnt);
+    let cntTmp = [...cnt];
+    console.log(i);
+    cntTmp[i] += 1;
+    setCnt(cntTmp);
+
+    console.log(cntTmp);
   };
+  // 가나다순 정렬
   function sorting() {
     let tmp2 = [...title];
     tmp2.sort((p1, p2) => p1.localeCompare(p2));
@@ -33,24 +38,21 @@ function App() {
           {/* {color:color} -> 작동 안함  */}
           <h3>BLOG</h3>
         </div>
-        <div className='list'>
-          <h3>
-            {title[0]}
-            <span style={{cursor: 'pointer'}} onClick={click}>
-              😎
-            </span>
-            {cnt}
-          </h3>
-          <p>7월 14일 발행</p>
-        </div>
-        <div className='list'>
-          <h3>{title[1]}</h3>
-          <p>7월 14일 발행</p>
-        </div>
-        <div className='list'>
-          <h3>{title[2]}</h3>
-          <p>7월 14일 발행</p>
-        </div>
+        {title.map((v, i) => {
+          return (
+            <div className='list' key={i}>
+              <h3>
+                {v}
+                <span style={{cursor: 'pointer'}} onClick={click.bind(null, i)}>
+                  ♥️
+                </span>
+                {cnt[i]}
+              </h3>
+              <p>7월 14일 발행</p>
+            </div>
+          );
+        })}
+
         <button onClick={sorting}>정렬</button>
         {modal ? <Modal modal={modal} setModal={setModal} /> : null}
         <button onClick={modalUp}>모달창 열기</button>
