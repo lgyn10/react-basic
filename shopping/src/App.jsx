df421c9fefd44d121437e7cb52a606ae1a7b4e1d
ChanGyun Lee <lgyn10@naver.com>
2023-07-17 20:20:41 +0900

    메인 이미지, 제품 이미지(3) 반응형 배치

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -1,9 +1,31 @@
+import {Container, Image, Nav, Navbar} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <>
-      <div></div>
+      <div className='App'>
+        <Navbar bg='dark' data-bs-theme='dark'>
+          <Container>
+            <Navbar.Brand href='#home'>GYUN-MALL</Navbar.Brand>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>Home</Nav.Link>
+              <Nav.Link href='#features'>Products</Nav.Link>
+              <Nav.Link href='#pricing'>contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div className='main-bg'></div>
+        <div>
+          <div className='prdt-container'>
+            <Image className='product-img' src='/public/chalk.png' rounded />
+            <Image className='product-img' src='/public/shoes.png' rounded />
+            <Image className='product-img' src='/public/strap.jpg' rounded />
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 }
