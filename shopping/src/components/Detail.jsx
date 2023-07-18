dc6715376dc5d6d53815e8788df6b37d00a8ce96
ChanGyun Lee <lgyn10@naver.com>
2023-07-18 20:23:51 +0900

    useEffect 예제

--- shopping/src/components/Detail.jsx
+++ shopping/src/components/Detail.jsx
@@ -1,14 +1,24 @@
 /* eslint-disable react/prop-types */
+import {useEffect, useState} from 'react';
 import {useParams} from 'react-router';
 import styled from 'styled-components';
 
 const Detail = (props) => {
+  const [cnt, setCnt] = useState(0);
+  const [isShow, setIsShow] = useState(true);
+  useEffect(() => {
+    // mount, update 일 때
+    console.log('useEffect');
+    setTimeout(() => setIsShow(0), 3000);
+  });
+
   let {paramId} = useParams();
   let product = props.products.filter((e) => e.id == paramId)[0];
-  console.log(product);
-  console.log(paramId);
+  // console.log(product);
+  // console.log(paramId);
   return (
     <div className='container'>
+      {isShow ? <div className='alert alert-warning'>3초 이내에 구매 시 20% 할인!</div> : null}
       <div className='row'>
         <div className='col-md-6'>
           <img src={'/' + product.img} width='100%' />
@@ -18,13 +28,16 @@ const Detail = (props) => {
           <p>{product.content}</p>
           <p>{product.price}원</p>
           <StyledBtn color='lightgreen' className='btn'>
-            주문하기
+            네이버 페이
           </StyledBtn>
           <StyledBtn color='lightblue' className='btn'>
-            주문하기
+            토스 페이
           </StyledBtn>
           <StyledBtn color='red' className='btn'>
-            주문하기
+            페이북
+          </StyledBtn>
+          <StyledBtn color='lightyellow' className='btn' onClick={() => setCnt((prev) => prev + 1)}>
+            {cnt}
           </StyledBtn>
         </div>
       </div>
--- shopping/src/components/MdProducts.jsx
+++ shopping/src/components/MdProducts.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/prop-types */
 
 import {Image} from 'react-bootstrap';
+import {useNavigate} from 'react-router';
 
 const MdProducts = (props) => {
+  let navigate = useNavigate();
   return (
     <div key={props.product.id}>
-      <Image className='product-img' src={props.product.img} rounded />
+      <Image
+        style={{cursor: 'pointer'}}
+        onClick={() => {
+          navigate('/detail/' + props.product.id);
+        }}
+        className='product-img'
+        src={props.product.img}
+        rounded
+      />
       <h4>{props.product.title}</h4>
       <p>{props.product.content}</p>
       <p>가격: {props.product.price}</p>
